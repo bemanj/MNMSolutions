598093b9a20256352b375ea3b723876d9baaf51e
rjohn <rjohn-@users.noreply.github.com>
2017-09-12 18:04:52 +0800

    added models from DB and OrderDetailsController

---
+++ MNMSolutions.DAL/DB/Test/Region.cs
@@ -0,0 +1,20 @@
+namespace MNMSolutions.DAL.DB.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Region")]
+    public partial class Region
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RegionId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RegionDescription { get; set; }
+
+    }
+}
---
+++ MNMSolutions.Web.Api/Controllers/OrderDetailsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MNMSolutions.DAL.DB.Dev;
+using MNMSolutions.DAL.DB.Test;
+
+namespace MNMSolutions.Web.Api.Controllers
+{
+    public class OrderDetailsController : ApiController
+    {
+        private readonly MNMSolutionsDevDBEntities _db = new MNMSolutionsDevDBEntities();
+
+        // GET: api/OrderDetails
+        public IQueryable<OrderDetailList> GetOrderDetails()
+        {
+            var orderList = _db.OrderDetails
+                .Select(o => new OrderDetailList()
+                {
+                    OrderId = o.OrderID,
+                    UnitPrice = o.UnitPrice,
+                    Quantity = o.Quantity,
+                    Discount = o.Discount
+                });
+            return orderList;
+        }
+
+        // GET: api/OrderDetails/5
+        [ResponseType(typeof(OrderDetail))]
+        public async Task<IHttpActionResult> GetOrderDetail(int id)
+        {
+            OrderDetail orderDetail = await _db.OrderDetails.FindAsync(id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orderDetail);
+        }
+
+        // PUT: api/OrderDetails/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutOrderDetail(int id, OrderDetail orderDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != orderDetail.OrderID)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(orderDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/OrderDetails
+        [ResponseType(typeof(OrderDetail))]
+        public async Task<IHttpActionResult> PostOrderDetail(OrderDetail orderDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.OrderDetails.Add(orderDetail);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderDetailExists(orderDetail.OrderID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = orderDetail.OrderID }, orderDetail);
+        }
+
+        // DELETE: api/OrderDetails/5
+        [ResponseType(typeof(OrderDetail))]
+        public async Task<IHttpActionResult> DeleteOrderDetail(int id)
+        {
+            OrderDetail orderDetail = await _db.OrderDetails.FindAsync(id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            _db.OrderDetails.Remove(orderDetail);
+            await _db.SaveChangesAsync();
+
+            return Ok(orderDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderDetailExists(int id)
+        {
+            return _db.OrderDetails.Count(e => e.OrderID == id) > 0;
+        }
+    }
+}
