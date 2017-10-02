c6f27e2bdf9c0807ed2527629ad62535f48075b3
rjohn <rjohn-@users.noreply.github.com>
2017-10-03 06:54:09 +0800

    added BLL

---
+++ MNMSolutions.BLL/ProductList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MNMSolutions.DAL.DB.Dev;
+
+namespace MNMSolutions.BLL
+{
+    public class ProductList
+    {
+        private readonly MNMSolutionsDevDBEntities _db = new MNMSolutionsDevDBEntities();
+
+        // GET: api/ProductOnes
+        public IQueryable<ProductOne> GetProductOnes()
+        {
+            return _db.ProductOnes;
+        }
+    }
+}
