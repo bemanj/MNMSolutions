601ad2f2d309e0d97c8abf12fa436821b5564f1d
Robert John Millano <robertjohnmillano@outlook.com>
2017-10-17 11:01:23 -0500

    update web api of sales order

--- MNMSolutions.DAL/DB/Dev/Model1.Designer.cs
+++ MNMSolutions.DAL/DB/Dev/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\rober\source\repos\MNMSolutions\MNMSolutions.DAL\DB\Dev\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\rm004134263\source\repos\MNMSolutions\MNMSolutions.DAL\DB\Dev\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
