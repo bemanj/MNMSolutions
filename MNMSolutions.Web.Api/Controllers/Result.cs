ffb1d0a0596246de247c842a8a6577d78b7e7011
Robert John Millano <RM004134263@mercury.local>
2017-10-26 22:20:16 +0800

    cleaned master repo

--- MNMSolutions.Web.Api/Controllers/Result.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MNMSolutions.Web.Api.Controllers
-{
-    public class Result
-    {
-        public string Name { get; set; }
-        public string Category { get; set; }
-    }
-}
--- MNMSolutions.Web.Presentation/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MNMSolutions.Web.Presentation.MvcApplication" Language="C#" %>
