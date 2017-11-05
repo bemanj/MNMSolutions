7ae39d97e360edaba23e9f85b7172b100bb80f49
rjohn <rjohn-@users.noreply.github.com>
2017-11-05 10:26:50 +0800

    soh

---
+++ MNMSolutions.DAL/Models/Sales/SohInsert.cs
@@ -0,0 +1,12 @@
+﻿namespace MNMSolutions.DAL.Models.Sales
+{
+    public class SohInsert
+    {
+        //public short Customer { get; set; }
+        //public string OnlineOrderFlag { get; set; }
+        public short SubTotal { get; set; }
+        public short TaxAmt  { get; set; }
+        public short Freight { get; set; }
+        //public string Comment { get; set; }
+    }
+}
---
+++ MNMSolutions.DAL/Models/Sales/SohUpdate.cs
@@ -0,0 +1,14 @@
+﻿namespace MNMSolutions.DAL.Models.Sales
+{
+    public class SohUpdate
+    {
+        public short SalesOrderId { get; set; }
+        public short Customer { get; set; }
+        public string OnlineOrderFlag { get; set; }
+        public short SubTotal { get; set; }
+        public short TaxAmt  { get; set; }
+        public short Freight { get; set; }
+        public string Comment { get; set; }
+        public bool Fulfilled { get; set; }
+    }
+}
